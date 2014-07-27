a9177cbeffaf515d2aac2d7c3b6cc236b41c0476
deepak jangir <deepakjangir07@gmail.com>
2014-07-27 15:42:19 +0530

    =>APIPost File

--- src/co/uk/tusksolutions/tchat/android/api/APIPostFile.java
+++ src/co/uk/tusksolutions/tchat/android/api/APIPostFile.java
@@ -1,32 +1,27 @@
 package co.uk.tusksolutions.tchat.android.api;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.CoreProtocolPNames;
-import org.apache.http.params.HttpParams;
-import org.jivesoftware.smack.util.StringUtils;
-
 import co.uk.tusksolutions.tchat.android.TChatApplication;
-import co.uk.tusksolutions.tchat.android.activities.ChatActivity;
 import co.uk.tusksolutions.tchat.android.constants.Constants;
 import co.uk.tusksolutions.tchat.android.xmpp.XMPPChatMessageManager;
-import co.uk.tusksolutions.utility.Utility;
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.text.Html;
 import android.util.Log;
 
 @SuppressWarnings("deprecation")
@@ -63,10 +58,10 @@ public class APIPostFile {
 			boolean apiResult = false;
 
 			try {
-				File file = new File(selectedFile);
+				//File file = new File(selectedFile);
 
 				try {
-					link = UploadFile(sender.replace("@"+Constants.CURRENT_SERVER,""), receiver.replace("@"+Constants.CURRENT_SERVER,""), file);
+					link = postFile(sender.replace("@"+Constants.CURRENT_SERVER,""), receiver.replace("@"+Constants.CURRENT_SERVER,""), selectedFile);
 					Log.e("upload file link ", link);
 				} catch (UnsupportedEncodingException e) {
 					// TODO Auto-generated catch block
@@ -107,64 +102,124 @@ public class APIPostFile {
 		}
 	}
 
-	@SuppressWarnings("deprecation")
-	public String UploadFile(String sender, String receiver, File filepath)
-			throws UnsupportedEncodingException {
-		String responsed = null;
-		Log.e("File Uploaded", "In file upload " + filepath.toString());
-		DefaultHttpClient httpclient;
-		HttpParams params = new BasicHttpParams();
-		params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION,
-				HttpVersion.HTTP_1_1);
-		httpclient = new DefaultHttpClient(params);
-		HttpPost httpPost = new HttpPost(Constants.HTTP_SCHEME
+
+	
+	public static String postFile(String sender, String receiver, String fileName) throws Exception {
+       Log.e("File send",fileName);
+	    HttpClient client = new DefaultHttpClient();
+	    HttpPost post = new HttpPost(Constants.HTTP_SCHEME
 				+ Constants.CURRENT_SERVER + Constants.UPLOAD_FILE_ENDPOINT);
+	    MultipartEntityBuilder builder = MultipartEntityBuilder.create();        
+	    builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 
-		MultipartEntity builder = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+	    final File file = new File(fileName);
+	    FileBody fb = new FileBody(file);
 
-		builder.addPart("sender", new StringBody(sender));
-		builder.addPart("receiver", new StringBody(receiver));
+	    builder.addPart("upfile", fb);  
+	    builder.addTextBody("sender", sender);
+	    builder.addTextBody("receiver", receiver);
+	  
+	    final HttpEntity yourEntity = builder.build();
 
-		if (filepath != null && filepath.exists())
-			builder.addPart("upfile", new FileBody(filepath));
+	    class ProgressiveEntity implements HttpEntity {
+	        @Override
+	        public void consumeContent() throws IOException {
+	            yourEntity.consumeContent();                
+	        }
+	        @Override
+	        public InputStream getContent() throws IOException,
+	                IllegalStateException {
+	            return yourEntity.getContent();
+	        }
+	        @Override
+	        public Header getContentEncoding() {             
+	            return yourEntity.getContentEncoding();
+	        }
+	        @Override
+	        public long getContentLength() {
+	            return yourEntity.getContentLength();
+	        }
+	        @Override
+	        public Header getContentType() {
+	            return yourEntity.getContentType();
+	        }
+	        @Override
+	        public boolean isChunked() {             
+	            return yourEntity.isChunked();
+	        }
+	        @Override
+	        public boolean isRepeatable() {
+	            return yourEntity.isRepeatable();
+	        }
+	        @Override
+	        public boolean isStreaming() {             
+	            return yourEntity.isStreaming();
+	        } // CONSIDER put a _real_ delegator into here!
 
-		httpPost.setEntity(builder);
+	        @Override
+	        public void writeTo(OutputStream outstream) throws IOException {
 
-		HttpResponse httpResponse = null;
-		try {
-			httpResponse = httpclient.execute(httpPost);
-		} catch (ClientProtocolException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			httpResponse = httpclient.execute(httpPost);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		InputStream inputStream = null;
-		try {
-			inputStream = httpResponse.getEntity().getContent();
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Utility utility = new Utility();
-		if (inputStream != null)
+	            class ProxyOutputStream extends FilterOutputStream {
+	                /**
+	                 * @author Stephen Colebourne
+	                 */
 
-			responsed = utility.convertStreamToString(inputStream);
-		System.out.println("Response: " + responsed);
-		return responsed;
+	                public ProxyOutputStream(OutputStream proxy) {
+	                    super(proxy);    
+	                }
+	                public void write(int idx) throws IOException {
+	                    out.write(idx);
+	                }
+	                public void write(byte[] bts) throws IOException {
+	                    out.write(bts);
+	                }
+	                public void write(byte[] bts, int st, int end) throws IOException {
+	                    out.write(bts, st, end);
+	                }
+	                public void flush() throws IOException {
+	                    out.flush();
+	                }
+	                public void close() throws IOException {
+	                    out.close();
+	                }
+	            } // CONSIDER import this class (and risk more Jar File Hell)
+
+	            class ProgressiveOutputStream extends ProxyOutputStream {
+	                public ProgressiveOutputStream(OutputStream proxy) {
+	                    super(proxy);
+	                }
+	                public void write(byte[] bts, int st, int end) throws IOException {
+
+	                    // FIXME  Put your progress bar stuff here!
+
+	                    out.write(bts, st, end);
+	                }
+	            }
+
+	            yourEntity.writeTo(new ProgressiveOutputStream(outstream));
+	        }
+
+	    };
+	    ProgressiveEntity myEntity = new ProgressiveEntity();
+
+	    post.setEntity(myEntity);
+	    HttpResponse response = client.execute(post);        
+
+	    return getContent(response);
+
+	} 
+
+	public static String getContent(HttpResponse response) throws IOException {
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+	    String body = "";
+	    String content = "";
+
+	    while ((body = rd.readLine()) != null) 
+	    {
+	        content += body + "\n";
+	    }
+	    return content.trim();
 	}
+
 
 }
