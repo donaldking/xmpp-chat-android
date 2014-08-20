ec3c39c02456f6f13575f834a17e0d766472711c
Donald King <donaldking247@gmail.com>
2014-08-20 11:43:33 +0100

    ignore

--- src/co/uk/tusksolutions/tchat/android/GCMIntentService.java
+++ src/co/uk/tusksolutions/tchat/android/GCMIntentService.java
@@ -39,7 +39,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 	protected void onMessage(final Context context, Intent intent) {
 
 		String jsonString = intent.getStringExtra("message");
-		
+
 		try {
 			JSONObject object = new JSONObject(jsonString);
 			if (jsonString.length() > 0) {
@@ -76,9 +76,9 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 	}
 
-	public void processNotification(JSONObject object){
+	public void processNotification(JSONObject object) {
 		try {
-			
+
 			Bundle b = new Bundle();
 
 			if (object.getString("messageType").equalsIgnoreCase("CHAT")) {
@@ -101,8 +101,8 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 				Intent i = new Intent();
 				i.putExtra("chatFromFriendBundle", b);
-				
-				//this.saveNormalChatMessageToDb(b);
+
+				// this.saveNormalChatMessageToDb(b);
 
 				// Send TO_USER notification manager
 				new XMPPNotificationManager().sendNormalChatNotification(i);
@@ -151,8 +151,47 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 			} else if (object.getString("messageType").equalsIgnoreCase(
 					"CHAT_ROOM")) {
-				b.putString("roomJid", object.getString("sender")
-						+ "@conference." + Constants.CURRENT_SERVER);
+
+				Log.d(TAG, "T: " + object.toString());
+				Bundle groupChatBundle = new Bundle();
+				groupChatBundle.putString("roomJid",
+						object.getString("roomJid"));
+				groupChatBundle.putString("resource",
+						object.getString("sender"));
+				groupChatBundle.putString("roomName",
+						object.getString("sender_display_name"));
+				groupChatBundle.putString("senderJid",
+						object.getString("sender") + "@"
+								+ Constants.CURRENT_SERVER);
+				groupChatBundle.putString(
+						"senderName",
+						TChatApplication.getRosterModel().getBuddyName(
+								object.getString("sender") + "@"
+										+ Constants.CURRENT_SERVER));
+
+				/*
+				 * Image & File notifications added
+				 */
+				String last_message = object.getString("message");
+				if (last_message.contains("<img src")) {
+					last_message = "Image";
+				} else if (last_message.contains("<a target")) {
+					last_message = "File";
+				}
+
+				groupChatBundle.putString("message", last_message);
+				groupChatBundle.putString("messageType", "CHAT_ROOM");
+
+				Intent i = new Intent();
+				i.putExtra("groupChatFromRoomBundle", groupChatBundle);
+
+				Log.d(TAG,
+						"message received: "
+								+ groupChatBundle.getString("message"));
+
+				// Send TO_USER notification manager
+				new XMPPNotificationManager().sendGroupChatNotification(i);
+
 			}
 
 		} catch (JSONException e) {
@@ -178,63 +217,59 @@ public class GCMIntentService extends GCMBaseIntentService {
 	protected void onError(Context context, String errorId) {
 
 	}
-	
-	/*private void saveNormalChatMessageToDb(Bundle bundle) {
-		/*
-		 * Insert received message to db
-		 *
-		String resource = StringUtils.parseResource(packet.getFrom());
-		ChatMessagesModel mChatMessageModel = new ChatMessagesModel();
-		String mid = packet.getPacketID();
-		/*
-		 * If Message from Other Resource and get the carbon message
-		 *
-		if (message.getBody().contains("|s|")) {
 
-			String MessageParse[] = message.getBody().split("\\|s\\|");
-			String buddyJID = MessageParse[0];
-			String last_message = MessageParse[1];
-			mChatMessageModel.saveMessageToDB(TChatApplication.getCurrentJid(),
-					buddyJID, resource,
-					StringUtils.parseName(packet.getFrom()), last_message, 0,
-					"CHAT", System.currentTimeMillis(), 1, mid);
-
-		} else {
-
-			mChatMessageModel.saveMessageToDB(TChatApplication.getCurrentJid(),
-					StringUtils.parseBareAddress(packet.getFrom()), resource,
-					StringUtils.parseName(packet.getFrom()), message.getBody(),
-					0, "CHAT", System.currentTimeMillis(), 1, mid);
-		}
-	}*/
+	/*
+	 * private void saveNormalChatMessageToDb(Bundle bundle) { /* Insert
+	 * received message to db
+	 * 
+	 * String resource = StringUtils.parseResource(packet.getFrom());
+	 * ChatMessagesModel mChatMessageModel = new ChatMessagesModel(); String mid
+	 * = packet.getPacketID(); /* If Message from Other Resource and get the
+	 * carbon message
+	 * 
+	 * if (message.getBody().contains("|s|")) {
+	 * 
+	 * String MessageParse[] = message.getBody().split("\\|s\\|"); String
+	 * buddyJID = MessageParse[0]; String last_message = MessageParse[1];
+	 * mChatMessageModel.saveMessageToDB(TChatApplication.getCurrentJid(),
+	 * buddyJID, resource, StringUtils.parseName(packet.getFrom()),
+	 * last_message, 0, "CHAT", System.currentTimeMillis(), 1, mid);
+	 * 
+	 * } else {
+	 * 
+	 * mChatMessageModel.saveMessageToDB(TChatApplication.getCurrentJid(),
+	 * StringUtils.parseBareAddress(packet.getFrom()), resource,
+	 * StringUtils.parseName(packet.getFrom()), message.getBody(), 0, "CHAT",
+	 * System.currentTimeMillis(), 1, mid); } }
+	 */
 
 	/**
 	 * Issues a notification to inform the user that server has sent a message.
-	 *
-	private static void generateNotification(Context context, String message) {
-		int icon = R.drawable.ic_launcher;
-		long when = System.currentTimeMillis();
-		NotificationManager notificationManager = (NotificationManager) context
-				.getSystemService(Context.NOTIFICATION_SERVICE);
-		Notification notification = new Notification(icon, message, when);
-
-		String title = context.getString(R.string.app_name);
-
-		Intent notificationIntent = new Intent(context, MainActivity.class);
-		// set intent so it does not start a new activity
-		notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-				| Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		PendingIntent intent = PendingIntent.getActivity(context, 0,
-				notificationIntent, 0);
-		notification.setLatestEventInfo(context, title, message, intent);
-		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-
-		// Play default notification sound
-		notification.defaults |= Notification.DEFAULT_SOUND;
-
-		// Vibrate if vibrate is enabled
-		notification.defaults |= Notification.DEFAULT_VIBRATE;
-		notificationManager.notify(0, notification);
-
-	}*/
+	 * 
+	 * private static void generateNotification(Context context, String message)
+	 * { int icon = R.drawable.ic_launcher; long when =
+	 * System.currentTimeMillis(); NotificationManager notificationManager =
+	 * (NotificationManager) context
+	 * .getSystemService(Context.NOTIFICATION_SERVICE); Notification
+	 * notification = new Notification(icon, message, when);
+	 * 
+	 * String title = context.getString(R.string.app_name);
+	 * 
+	 * Intent notificationIntent = new Intent(context, MainActivity.class); //
+	 * set intent so it does not start a new activity
+	 * notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+	 * Intent.FLAG_ACTIVITY_SINGLE_TOP); PendingIntent intent =
+	 * PendingIntent.getActivity(context, 0, notificationIntent, 0);
+	 * notification.setLatestEventInfo(context, title, message, intent);
+	 * notification.flags |= Notification.FLAG_AUTO_CANCEL;
+	 * 
+	 * // Play default notification sound notification.defaults |=
+	 * Notification.DEFAULT_SOUND;
+	 * 
+	 * // Vibrate if vibrate is enabled notification.defaults |=
+	 * Notification.DEFAULT_VIBRATE; notificationManager.notify(0,
+	 * notification);
+	 * 
+	 * }
+	 */
 }
--- src/co/uk/tusksolutions/tchat/android/TChatApplication.java
+++ src/co/uk/tusksolutions/tchat/android/TChatApplication.java
@@ -54,8 +54,8 @@ public class TChatApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Constants.CURRENT_SERVER = Constants.DEVELOPMENT_SERVER;
-		//Constants.CURRENT_SERVER = Constants.STAGING_SERVER;
+		//Constants.CURRENT_SERVER = Constants.DEVELOPMENT_SERVER;
+		Constants.CURRENT_SERVER = Constants.STAGING_SERVER;
 		// Constants.CURRENT_SERVER = Constants.PRODUCTION_SERVER;
 		Constants.PROXY_SERVER = Constants.HTTP_SCHEME
 				+ Constants.CURRENT_SERVER + Constants.PROXY_PATH;
